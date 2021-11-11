152655aa6eaf15f2f79943abd213c7dcd26439f5
Eric Fontaine <ericfontainejazz@gmail.com>
2021-11-10 20:25:54 -0500

    change from qt5 to qt6 and hope

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,15 @@
 #include <sqlite3.h>
 #include <stdio.h>
 #include <fmt/core.h>
+#include <QCoreApplication>
 
-// main function!
-int main()
+using namespace std;
+int main(int argc, char *argv[])
 {
+//    QCoreApplication a(argc, argv);
+
     fmt::print("Hello, world!\n");
     printf("%s\n", sqlite3_libversion());
-    return 0;
+
+    return 0;// a.exec();
 }

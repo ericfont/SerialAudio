d660ca93e558c93b7c8eb2abf9914071c5b59b50
Eric Fontaine <ericfontainejazz@gmail.com>
2021-11-11 21:59:53 -0500

    attempt build qt5 again on mac

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,38 @@
-#include <iostream>
-#include <thread>
+/*
+ * File:   main.cpp
+ * Author: fede.tft
+ *
+ * Created on September 10, 2009, 10:50 AM
+ */
 
-#include "BufferedAsyncSerial.h"
+#include <iostream>
+#include "TimeoutSerial.h"
 
 using namespace std;
+using namespace boost;
 
 int main(int argc, char* argv[])
 {
     try {
-        BufferedAsyncSerial serial("/dev/ttyUSB0",115200);
 
-        //Return immediately. String is written *after* the function returns,
-        //in a separate thread.
+        TimeoutSerial serial("/dev/ttyUSB0",115200);
+        serial.setTimeout(posix_time::seconds(5));
+
+        //Text test
         serial.writeString("Hello world\r\n");
-
-        //Simulate doing something else while the serial device replies.
-        //When the serial device replies, the second thread stores the received
-        //data in a buffer.
-        this_thread::sleep_for(chrono::seconds(2));
-
-        //Always returns immediately. If the terminator \r\n has not yet
-        //arrived, returns an empty string.
         cout<<serial.readStringUntil("\r\n")<<endl;
 
+        //Binary test
+        unsigned char values[]={0xde,0xad,0xbe,0xef};
+        serial.write(reinterpret_cast<char*>(values),sizeof(values));
+        serial.read(reinterpret_cast<char*>(values),sizeof(values));
+        for(unsigned int i=0;i<sizeof(values);i++)
+        {
+            cout<<static_cast<int>(values[i])<<endl;
+        }
+
         serial.close();
-  
+
     } catch(boost::system::system_error& e)
     {
         cout<<"Error: "<<e.what()<<endl;
